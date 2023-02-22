2341df731abebf736d8a536b33ad55bb2e7121b8
Naqeeb4404 <naqeeblat@gmail.com>
2023-02-23 07:04:50 +0800

    b

---
+++ practise10/while2.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+int main()
+{
+    int cout =1, sum = 0;
+    while (count <= 10)
+    { sum += count; // same as sum = sum + count
+    count = count + 1;;
+    }
+    std::cout << "Sum of 1 to 10 = " <<sum <<std::endl;
+    return 0;
+}
