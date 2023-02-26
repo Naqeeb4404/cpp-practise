18232372f56a0d9d8062774ecf7b5ab904a4bf4c
Naqeeb4404 <naqeeblat@gmail.com>
2023-02-26 14:37:48 +0800

    j

---
+++ practise13/elseif2.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+int main()
+{
+    int choice =0;
+    std::cout << "Enter:" << std::endl << "1 - for Coffee."<< std::endl
+            <<"2 - FOR TEA." <<std::endl
+            std::cin >> choice;
+            if (choice == 1)
+            {
+                std::cout << "Your coffee is on its way. Thank you.";
+            }
+            else if (choice ==2)
+}
