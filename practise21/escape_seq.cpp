e605985c7f966aa4cad118d37c0076300d2b2e72
Naqeeb4404 <naqeeblat@gmail.com>
2023-03-03 14:59:48 +0800

    a

---
+++ practise21/escape_seq.cpp
@@ -0,0 +1,8 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    cout<<"this will be followed by a newline \n and after this i want a horizontal tab \t. Now here i want a backflash \\ and i want to write this word in \"double quote\"
+    and this in \'single quote\'. Let us see if all works correctly \? Okay\by ";
+    return 0;
+}
