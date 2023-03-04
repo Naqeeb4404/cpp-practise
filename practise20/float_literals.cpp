9c6c51a424bbb39fc4859ca6970602b595de4015
Naqeeb4404 <naqeeblat@gmail.com>
2023-03-04 15:41:02 +0800

    s

--- practise20/float_literals.cpp
+++ practise20/float_literals.cpp
@@ -1,10 +1,9 @@
-#include<iostream>
-int main(){
-    float m,m,o,p
-    n = 3.14159
-    m = 3.13159E2
-    o = 3.14159e3
-    p = 3.14159E-1
-    cout<<n<<endl<<m<<endl<<o<<endl<<p;
+#include <iostream>
+int main()
+{
+    float m, m, o, p n = 3.14159 m = 3.13159E2 o = 3.14159e3 p = 3.14159E-1 cout << n << endl
+                                                                                 << m << endl
+                                                                                 << o << endl
+                                                                                 << p;
     return 0;
 }
--- practise21/escape_seq.cpp
+++ practise21/escape_seq.cpp
@@ -1,8 +1,8 @@
-#include<iostream>
+#include <iostream>
 using namespace std;
 
-int main(){
-    cout<<"this will be followed by a newline \n and after this i want a horizontal tab \t. Now here i want a backflash \\ and i want to write this word in \"double quote\"
-    and this in \'single quote\'. Let us see if all works correctly \? Okay\by ";
-    return 0;
+int main()
+{
+    cout << "this will be followed by a newline \n and after this i want a horizontal tab \t. Now here i want a backflash \\ and i want to write this word in \"double quote\"
+        and this in \'single quote\'. Let us see if all works correctly \? Okay\by "; return 0;
 }
