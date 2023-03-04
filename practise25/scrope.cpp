13af1852653432735b7d2cecfa131fed4d66db82
Naqeeb4404 <naqeeblat@gmail.com>
2023-03-04 18:34:43 +0800

    s

---
+++ practise25/scrope.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+int another_func();
+int main()
+{
+    int sum = 0;
+    for (int val = 1; val <= 10; ++val)
+}
+sum = sum + val;
+}
+cout << "Sum of 1 to 10 inclusive is " << sum << endl;
+another_func();
+return 0;
+}
